e8068794a58fda64d9895ac94e7c324a00ec4e73
Marcel <mvelasquez2106@gmail.com>
2025-07-02 11:31:31 +0200

    Window Creation with some base-classes

---
+++ engine/src/bin/main.rs
@@ -0,0 +1,14 @@
+use engine::state::*;
+
+struct App {}
+
+impl EngineEvent for App
+{
+    fn update(&mut self, _dt: f64) {}
+    fn render(&self) {}
+}
+
+fn main()
+{
+    pollster::block_on(game_loop(Box::new(App{})))
+}

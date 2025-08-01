5e3f2bcae994ad6711cd5e44fc48489408ba843c
Marcel <mvelasquez2106@gmail.com>
2025-08-01 09:54:27 +0200

    FontAtlas Struct

--- engine/src/text.rs
+++ engine/src/text.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use ab_glyph::{point, Font, FontArc, PxScale};
 use anyhow::{Ok, anyhow};
 
@@ -30,4 +32,60 @@ pub fn rasterize_char(font_path: &str, char: char) -> std::result::Result<(Vec<u
     });
 
     Ok((bitmap, width, height))
+}
+
+// pub struct TextHandler
+// {
+//     fonts: Vec<FontArc> // Not sure if this is the best way, but for now it's fine
+// }
+
+// impl TextHandler
+// {
+//     pub fn new(font_paths: Vec<&str>) -> std::result::Result<TextHandler, anyhow::Error>
+//     {
+//         let mut fonts: Vec<FontArc> = Vec::new();
+
+//         for path in font_paths
+//         {
+//             let font_data = std::fs::read(path)?;
+//             let font = FontArc::try_from_vec(font_data)?;
+//             fonts.push(font);
+//         }
+
+//         Ok(TextHandler
+//         { 
+//             fonts 
+//         })
+//     }
+
+//     pub fn add_font(&mut self, font_path: &str) -> std::result::Result<usize, anyhow::Error> // returns id of font (just vector-position)
+//     {
+//         let font_data = std::fs::read(font_path)?;
+//         let font = FontArc::try_from_vec(font_data)?;
+//         let id = self.fonts.len();
+//         self.fonts.push(font);
+//         Ok(id)
+//     }
+
+//     pub fn rasterize_text(text: &str, text_scale: f32) -> std::result::Result<(Vec<u8>, usize, usize), anyhow::Error>
+//     {
+//         let scale = PxScale::from(text_scale);
+
+
+//     }
+// }
+
+pub struct FontAtlas
+{
+    pub texture: wgpu::BindGroup,
+    pub glyphs: HashMap<char, Glyph>
+}
+
+pub struct Glyph
+{
+    pub uv_min: [f32; 2], // top left (uv coordinate in texture)
+    pub uv_max: [f32; 2], // top right ("-")
+    pub size: [f32; 2], // width and height of bitmap
+    // pub offset: [f32; 2], // offset of baseline (like down or Up, forp or l)
+    pub advance: f32, // Space after Glyph
 }
